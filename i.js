ff952003408fe2e50c7eee1cbca3370787eff6ad
Diogo Vieira da Silva <diogovieira1903@gmail.com>
2025-09-14 15:49:28 -0300

    Add files via upload

---
+++ i.js
@@ -0,0 +1,7 @@
+
+document.querySelector('.btn-olho').onclick = function() {
+    window.location.href = 'o.html'; // abre a página Olho-Laser
+}; 
+document.querySelector('.btn-joia').onclick = function() {
+    window.location.href = 'g.html'; // abre a página Olho-Laser
+};
